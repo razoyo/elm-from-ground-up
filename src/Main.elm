659986c979989f06ccd8d121133b1b5a170d0536
Paul Byrne <pbyrne@razoyo.com>
2019-10-12 14:25:32 -0500

    as far as I can tell, this is the absolute minimum amount of code required for an Elm Hello World

--- src/Main.elm
+++ src/Main.elm
@@ -1,1 +1,34 @@
-<h1>Hello World</h1>
+import Browser
+import Html exposing (Html)
+
+main =
+  Browser.sandbox
+    { init = init
+    , update = update
+    , view = view
+    }
+
+
+init : Model
+init = 
+  "Hello World"
+
+
+-- MODEL
+type alias Model =
+  String
+
+
+type alias Msg = 
+  String
+
+-- UPDATE
+update : Msg -> Model -> Model
+update msg model =
+  model
+
+
+-- VIEW
+view : Model -> Html.Html Model
+view model =
+  Html.text "Hello World"
