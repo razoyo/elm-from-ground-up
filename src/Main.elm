23d5291585912e797a26130f5c981acd5af63701
Csaba Kiss <csakis@gmail.com>
2019-12-29 13:26:04 -0700

    updated to elm 0.19.1

--- src/Main.elm
+++ src/Main.elm
@@ -1,92 +1,114 @@
+module Main exposing (Model, Msg(..), addNewItem, capitalizeMatchedItems, init, initialModel, main, update, view)
+
 import Browser
-import Html exposing ( Html, text, p, button, div, input, span, form )
-import Html.Events exposing ( onClick, onInput, onSubmit )
-import Html.Attributes exposing ( value, style )
+import Html exposing (Html, button, div, form, input, p, span, text)
+import Html.Attributes exposing (style, value)
+import Html.Events exposing (onClick, onInput, onSubmit)
+
 
 main =
-  Browser.sandbox
-    { init = init
-    , update = update
-    , view = view
-    }
+    Browser.sandbox
+        { init = init
+        , update = update
+        , view = view
+        }
 
 
 initialModel =
-  { items = [ "Hello World", "Here I Am" ]
-  , newItem = ""
-  }
+    { items = [ "Hello World", "Here I Am" ]
+    , newItem = ""
+    }
 
 
 init : Model
-init = 
-  initialModel
+init =
+    initialModel
+
 
 
 -- MODEL
+
+
 type alias Model =
-  { items : List String
-  , newItem : String 
-  }
+    { items : List String
+    , newItem : String
+    }
 
 
-type Msg = Capitalize String
-  | Reset
-  | UpdateNew String
-  | AddNew
-  | Delete String
+type Msg
+    = Capitalize String
+    | Reset
+    | UpdateNew String
+    | AddNew
+    | Delete String
 
 
 
 -- UPDATE
+
+
 update : Msg -> Model -> Model
 update msg model =
-  case msg of
+    case msg of
+        Capitalize item ->
+            { model | items = List.map (\x -> capitalizeMatchedItems x item) model.items }
 
-    Capitalize item ->
-      { model | items = List.map (\x -> capitalizeMatchedItems x item )  model.items }
+        Reset ->
+            initialModel
 
-    Reset ->
-      initialModel
+        UpdateNew item ->
+            { model | newItem = item }
 
-    UpdateNew item ->
-      { model | newItem = item }
+        AddNew ->
+            addNewItem model
 
-    AddNew ->
-      addNewItem model
-
-    Delete item ->
-      { model | items = List.filter (\x-> item /= x ) model.items }
+        Delete item ->
+            { model | items = List.filter (\x -> item /= x) model.items }
 
 
 addNewItem : Model -> Model
 addNewItem model =
-  { model | items = model.items ++ [ model.newItem ]
-  , newItem = ""
-  }
+    { model
+        | items = model.items ++ [ model.newItem ]
+        , newItem = ""
+    }
+
 
 capitalizeMatchedItems : String -> String -> String
 capitalizeMatchedItems choice match =
-  if choice == match then 
-    String.toUpper choice
-  else
-    choice
+    if choice == match then
+        String.toUpper choice
+
+    else
+        choice
+
 
 
 -- VIEW
+
+
 view : Model -> Html Msg
 view model =
-  div [] [ 
-    div [] ( List.map 
-             (\x -> p [] [ span [ onClick ( Capitalize x ) ] [ text x ]
-                         , span [ style "margin-left" "10px"
-                                , style "color" "red"
-                                , style "font-family" "sans-serif"
-                                , onClick ( Delete x ) ] [ text "x" ] 
-                         ]
-             ) model.items
-           )
-           , form [ onSubmit AddNew ] [ input [ value model.newItem, onInput UpdateNew ] []
-                  , button [ style "margin-left" "10px" ] [ text "submit" ]
-                  ] 
-    , button [ onClick Reset ] [ text "reset" ]
-  ]
+    div []
+        [ div []
+            (List.map
+                (\x ->
+                    p []
+                        [ span [ onClick (Capitalize x) ] [ text x ]
+                        , span
+                            [ style "margin-left" "10px"
+                            , style "color" "red"
+                            , style "font-family" "sans-serif"
+                            , onClick (Delete x)
+                            ]
+                            [ text "x" ]
+                        ]
+                )
+                model.items
+            )
+        , form [ onSubmit AddNew ]
+            [ input [ value model.newItem, onInput UpdateNew ] []
+            , button [ style "margin-left" "10px" ] [ text "submit" ]
+            ]
+        , button [ onClick Reset ] [ text "reset" ]
+        ]
