d85c428df1d1f70aa12960462190dac7b8cb6db4
Csaba Kiss <csakis@gmail.com>
2019-12-29 11:27:06 -0700

    updated to elm 0.19.1

--- src/Main.elm
+++ src/Main.elm
@@ -1,78 +1,100 @@
+module Main exposing (Model, Msg(..), capitalizeMatchedItems, init, initialModel, main, update, view)
+
 import Browser
-import Html exposing ( Html, text, p, button, div, input )
-import Html.Events exposing ( onClick, onInput )
-import Html.Attributes exposing ( value )
+import Html exposing (Html, button, div, input, p, text)
+import Html.Attributes exposing (value)
+import Html.Events exposing (onClick, onInput)
+
 
 main =
-  Browser.sandbox
-    { init = init
-    , update = update
-    , view = view
-    }
+    Browser.sandbox
+        { init = init
+        , update = update
+        , view = view
+        }
 
 
 initialModel =
-  { items = [ "Hello World", "Here I Am" ]
-  , newItem = ""
-  }
+    { items = [ "Hello World", "Here I Am" ]
+    , newItem = ""
+    }
 
 
 init : Model
-init = 
-  initialModel
+init =
+    initialModel
+
 
 
 -- MODEL
+
+
 type alias Model =
-  { items : List String
-  , newItem : String 
-  }
+    { items : List String
+    , newItem : String
+    }
 
 
-type Msg = Capitalize String
-  | Reset
-  | UpdateNew String
-  | AddNew
+type Msg
+    = Capitalize String
+    | Reset
+    | UpdateNew String
+    | AddNew
 
 
 
 -- UPDATE
+
+
 update : Msg -> Model -> Model
 update msg model =
-  case msg of
-    Capitalize item ->
-      { model | items = List.map (\x -> capitalizeMatchedItems x item )  model.items }
+    case msg of
+        Capitalize item ->
+            { model | items = List.map (\x -> capitalizeMatchedItems x item) model.items }
 
-    Reset ->
-      initialModel
+        Reset ->
+            initialModel
 
-    UpdateNew item ->
-      { model | newItem = item }
+        UpdateNew item ->
+            { model | newItem = item }
 
-    AddNew ->
-      { model | items = model.items ++ [ model.newItem ]
-      , newItem = ""
-      }
+        AddNew ->
+            { model
+                | items = model.items ++ [ model.newItem ]
+                , newItem = ""
+            }
 
 
 capitalizeMatchedItems : String -> String -> String
 capitalizeMatchedItems choice match =
-  if choice == match then 
-    String.toUpper choice
-  else
-    choice
-    
+    if choice == match then
+        String.toUpper choice
+
+    else
+        choice
+
+
 
 -- VIEW
+
+
 view : Model -> Html Msg
 view model =
-  div [] [ div [] ( List.map (\x -> 
-                    p [ onClick ( Capitalize x ) ] [ text x ] ) 
-                    model.items 
-                  )
-    , p [] [ input [ value model.newItem
-                   , onInput UpdateNew ] []
-           , button [ onClick AddNew ] [ text "submit" ]
-           ] 
-    , button [ onClick Reset ] [ text "undo" ]
-  ]
+    div []
+        [ div []
+            (List.map
+                (\x ->
+                    p [ onClick (Capitalize x) ] [ text x ]
+                )
+                model.items
+            )
+        , p []
+            [ input
+                [ value model.newItem
+                , onInput UpdateNew
+                ]
+                []
+            , button [ onClick AddNew ] [ text "submit" ]
+            ]
+        , button [ onClick Reset ] [ text "undo" ]
+        ]
